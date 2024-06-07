00511ee2c5e3ffa77b39be0e5123359e3618f867
AndreaChMaciel <andreachmaciel@gmail.com>
2024-06-07 16:06:58 -0600

    login nuevo

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonLabel, IonButton, IonAlert } from '@ionic/react';
+
+const Login: React.FC = () => {
+  const [ct_correo, setCorreo] = useState('');
+  const [ct_contrasena, setContrasena] = useState('');
+  const [error, setError] = useState('');
+
+  const validaLogin = async () => {
+    // Validar que los campos de correo y contraseña no estén vacíos
+    if (!ct_correo || !ct_contrasena) {
+      setError('Por favor, proporcione un correo y una contraseña');
+      return;
+    }
+
+    try {
+      const response = await axios.post('http://localhost:8000/api/login', {
+        ct_correo,
+        ct_contrasena
+      });
+
+      const { success, message } = response.data;
+
+      if (!success) {
+        setError(message);
+      } else {
+        setError('Inicio de sesión exitoso!!');
+        // Manejar el inicio de sesión exitoso, por ejemplo, redirigir a otra página
+      }
+    } catch (err) {
+      setError('Error al iniciar sesión');
+    }
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonItem>
+          <IonLabel position="stacked">Correo</IonLabel>
+          <IonInput
+            type="email"
+            value={ct_correo}
+            onIonChange={e => setCorreo(e.detail.value!)}
+          />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Contraseña</IonLabel>
+          <IonInput
+            type="password"
+            value={ct_contrasena}
+            onIonChange={e => setContrasena(e.detail.value!)}
+          />
+        </IonItem>
+        {error && <IonAlert
+          isOpen={!!error}
+          onDidDismiss={() => setError('')}
+          message={error}
+          buttons={['OK']}
+        />}
+        <IonButton expand="block" onClick={validaLogin}>Login</IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
